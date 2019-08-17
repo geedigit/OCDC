426d00fa9df1d875ee97193e2edaa19d93736297
geedigit <gabriel.jones@psy.ox.ac.uk>
2019-08-17 15:45:31 +0100

    Initial Commit

---
+++ data_fixer/statusUpdate.m
@@ -0,0 +1,6 @@
+function statusUpdate(msg)
+%STATUSUPDATE Summary of this function goes here
+%   Detailed explanation goes here
+
+end
+
---
+++ data_fixer/utils/xlwrite/firstlaunch_xlwrite.m
@@ -0,0 +1,10 @@
+%% Small demonstration on how to use XLWRITE
+
+%% Initialisation of POI Libs
+% Add Java POI Libs to matlab javapath
+javaaddpath('poi_library/poi-3.8-20120326.jar');
+javaaddpath('poi_library/poi-ooxml-3.8-20120326.jar');
+javaaddpath('poi_library/poi-ooxml-schemas-3.8-20120326.jar');
+javaaddpath('poi_library/xmlbeans-2.3.0.jar');
+javaaddpath('poi_library/dom4j-1.6.1.jar');
+javaaddpath('poi_library/stax-api-1.0.1.jar');
